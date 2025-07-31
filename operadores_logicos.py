323dd1503bb663b04e3e185a680a4af06e2e104d
Diego Ojeda <daom89@gmail.com>
2025-07-31 15:14:51 -0500

    Operadores Logicos

---
+++ operadores_logicos.py
@@ -0,0 +1,25 @@
+# print(5 == 5)          # True
+# print(5 == "5")        # False, los tipos son diferentes
+# print('a' > 'Z')       # True, las minúsculas tienen un valor ASCII mayor
+# print(f"{chr(97)} > {chr(90)}")
+
+# print("Hola" == 'Hola') # True
+# print("Hola" == "hola") # False
+
+# Mayor (>), Menor(<), Mayor o Igual (>=), Menor o Igual (<=) aplican las misma reglas
+# [1,2,3] == [1,2,3] # True
+# print([1, 2, 3] == (1, 2, 3)) # False
+# print(type([1,2,3])) # <class 'list'>
+# print(type((1,2,3))) # <class 'tuple'>
+#
+# [1,2,4] > [1,2,3] # True, compara elemento por elemento (lexicográficamente)
+#
+# 12 >= 12 # True
+# 1.3 > 1.1 # True
+# print(4 > "5") # Error
+# 'Z' > 'A' # True
+# print('a' > 'Z') # True, minúsculas tienen mayor valor ASCII
+# "Beta" > "Abaco" # True
+# [1, 2, 3] > [1, 2, 2] # True
+# [1, 2, 3] > (1, 2, 2) # Error
+
