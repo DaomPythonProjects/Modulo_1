7548f39ed6846328ccefb80c84627da36ea71260
Diego Ojeda <daom89@gmail.com>
2025-07-31 15:31:07 -0500

    Operadores Asignacion

---
+++ operadores_asignacion.py
@@ -0,0 +1,14 @@
+a = 1
+a += 5 # Incremento (+=) es equivalente (a = a + 5) ojo con (=+)
+a -= 5 # Decremento (-=) es equivalente (a = a - 5)
+a *= 3 # Multiplicacion (*=) es equivalente (a = a * 3)
+a /= 3 # Division (/=) es equivalente (a = a / 3)
+a %= 3 # Modulo (%=) es equivalente (a = a % 3)
+a **= 3 # Potenciacion (**=) es equivalente (a = a ** 3)
+a //= 3 # Division entera (//=)	es equivalente (a = a // 3)
+
+a &= 3 # Asignacion Bit a Bit AND (&=) es equivalente (a = a & 3)
+a |= 3 # Asignacion Bit a Bit OR (|=) es equivalente (a = a | 3)
+a ^= 3 # Asignacion Bit a Bit xOR (^=)	es equivalente (a = a ^ 3)
+a >>= 3 # (>>=) Desplazamiento Derecha Bit a Bit es equivalente (a = a >> 3)
+a <<= 3 # (<<=) Desplazamiento Derecha Bit a Bit es equivalente (a = a << 3)
